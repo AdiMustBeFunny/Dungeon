360b121fd5ca4079531e756e6ecccb79f71e5049
Adam Jankowski <adimustbefunny@gmail.com>
2019-01-04 00:43:41 +0100

    Forgot to delete few lines of code

--- KolorProject/Program.cs
+++ KolorProject/Program.cs
@@ -18,12 +18,7 @@ namespace KolorProject
         static void Main(string[] args)
         {
 
-            CorruptedSanta s = new CorruptedSanta();
-            s.mAttributes.Constitiution += 1;
-            s.mBasicStats.currentLevel += 3;
-            s.calculateBasicStats();
-            s.displayStats();
-            Console.ReadKey();
+
 
             GameController g = new GameController();
             g.initialize();
